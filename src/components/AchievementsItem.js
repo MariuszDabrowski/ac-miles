82c6873ef1caa1417f935f55cee728757e5603b9
Mariusz Dabrowski <wayrse@gmail.com>
2020-05-04 12:15:13 -0400

    Added ability to show connected and non connected tracks

--- src/components/AchievementsItem.js
+++ src/components/AchievementsItem.js
@@ -7,22 +7,27 @@ import './AchievementsItem.css';
 
 import awardsTrack1 from '../img/awards-1-blank.svg';
 import awardsTrack2 from '../img/awards-2-blank.svg';
+import awardsTrack2Connected from '../img/awards-2-blank-connected.svg';
 import awardsTrack3 from '../img/awards-3-blank.svg';
+import awardsTrack3Connected from '../img/awards-3-blank-connected.svg';
 import awardsTrack4 from '../img/awards-4-blank.svg';
+import awardsTrack4Connected from '../img/awards-4-blank-connected.svg';
 import awardsTrack5 from '../img/awards-5-blank.svg';
+import awardsTrack5Connected from '../img/awards-5-blank-connected.svg';
 import awardsTrack6 from '../img/awards-6-blank.svg';
+import awardsTrack6Connected from '../img/awards-6-blank-connected.svg';
 
 // -----------------------------------------------------
 // Map the images to labels we can reference in our code
 // -----------------------------------------------------
 
 const awardTracks = {
-  1: awardsTrack1,
-  2: awardsTrack2,
-  3: awardsTrack3,
-  4: awardsTrack4,
-  5: awardsTrack5,
-  6: awardsTrack6
+  1: { sequential: awardsTrack1, nonSequential: awardsTrack1 },
+  2: { sequential: awardsTrack2Connected, nonSequential: awardsTrack2 },
+  3: { sequential: awardsTrack3Connected, nonSequential: awardsTrack3 },
+  4: { sequential: awardsTrack4Connected, nonSequential: awardsTrack4 },
+  5: { sequential: awardsTrack5Connected, nonSequential: awardsTrack5 },
+  6: { sequential: awardsTrack6Connected, nonSequential: awardsTrack6 },
 }
 
 // ---------
@@ -41,6 +46,7 @@ class AchievementsItem extends React.Component {
   // ------
   
   render() {
+    const isSequential = (this.props.data['Sequential'] === 'Yes') ? 'sequential' : 'nonSequential';
 
     // ------
     // Return
@@ -58,7 +64,7 @@ class AchievementsItem extends React.Component {
           <img
           className="achievement__spaces-svg"
           alt=""
-          src={awardTracks[this.props.data['Num of Tiers']]} />
+          src={awardTracks[this.props.data['Num of Tiers']][isSequential]} />
         </div>
       </div>
     );
--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -8,24 +8,27 @@ import achievementDescriptions from '../data/achievementDescriptions.json';
 
 import track1 from '../img/track-straight-1.svg';
 import track2 from '../img/track-straight-2.svg';
+import track2Connected from '../img/track-straight-2-connected.svg';
 import track3 from '../img/track-straight-3.svg';
 import track3Connected from '../img/track-straight-3-connected.svg';
 import track4 from '../img/track-straight-4.svg';
+import track4Connected from '../img/track-straight-4-connected.svg';
 import track5 from '../img/track-straight-5.svg';
 import track5Connected from '../img/track-straight-5-connected.svg';
 import track6 from '../img/track-straight-6.svg';
+import track6Connected from '../img/track-straight-6-connected.svg';
 
 // -----------------------------------------------------
 // Map the images to labels we can reference in our code
 // -----------------------------------------------------
 
 const tracks = {
-  1: { sequential: null, nonSequential: track1 },
-  2: { sequential: null, nonSequential: track2 },
+  1: { sequential: track1, nonSequential: track1 },
+  2: { sequential: track2Connected, nonSequential: track2 },
   3: { sequential: track3Connected, nonSequential: track3 },
-  4: { sequential: null, nonSequential: track4 },
+  4: { sequential: track4Connected, nonSequential: track4 },
   5: { sequential: track5Connected, nonSequential: track5 },
-  6: { sequential: null, nonSequential: track6 },
+  6: { sequential: track6Connected, nonSequential: track6 },
 }
 
 // ---------
@@ -40,7 +43,7 @@ class CarouselItem extends React.Component {
   }
 
   render() {
-    const isSequential = (this.props.data.sequential === 'Yes') ? 'sequential' : 'nonSequential';
+    const isSequential = (this.props.data['Sequential'] === 'Yes') ? 'sequential' : 'nonSequential';
     const trackImage = tracks[this.props.data['Num of Tiers']][isSequential];
 
     // ------
